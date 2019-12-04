067d41d83271068c8d86dffff808f5c50e9ce149
Troy Christian <apostasy8@gmail.com>
2019-12-03 21:35:40 -0500

    SearchResult struct created

---
+++ ios-search/ios-search/SearchResult.swift
@@ -0,0 +1,9 @@
+//
+//  SearchResult.swift
+//  ios-search
+//
+//  Created by Lambda_School_Loaner_219 on 12/3/19.
+//  Copyright © 2019 Lambda_School_Loaner_219. All rights reserved.
+//
+
+import Foundation
